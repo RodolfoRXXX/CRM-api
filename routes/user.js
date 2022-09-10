cc63e50cdd796324ddef1a38d9cb4d44f015a9c8
Rodolfo <rodolfo.singular@gmail.com>
2022-09-10 17:47:54 -0300

    Rutas acceso login, register, forgot y verificador de cuenta terminados

--- routes/user.js
+++ routes/user.js
@@ -93,25 +93,31 @@ router.put('/verificate-user', async function(req, res, next){
     try{
         let {codeEmail, id} = req.body;
 
-        const checkEmail = `SELECT codeEmail FROM users WHERE id = ?`;
-        con.query(checkEmail, [id], (err, result, fields) => {
-            if (!result.length) {
+        const checkCode = `SELECT * FROM users WHERE id = ?`;
+        con.query(checkCode, [id], (err, result, fields) => {
+            if (result.length) {
                 //compruebo el codeEmail con el que me llegó
-                const sql = `INSERT INTO users (email, password, rol, codeEmail, active) VALUES (?, ?, ?, ?, ?)`;
-                con.query(sql, [email, hashed_password, rol, codeEmail, active], (err, result, fields) => {
+                if(result[0].codeEmail == codeEmail){
+                    const sql = `UPDATE users SET active= ? WHERE id = ?`;
+                con.query(sql, [1, id], (err, info, fields) => {
                     if (err) {
                         //error de conexion o para agregar el usuario
                         res.send({status: 0, data: err});
                     } else {
-                        let user = [{email: email, password: hashed_password, rol: rol, id: result.insertId, codeEmail: codeEmail, active: active}]
-                        //éxito al agregar el usuario
-                        let token = jwt.sign({data: user}, 'secret')
-                        res.send({status: 1, data: user, token: token});
+                        //EXITO!
+                        result[0].active = 1;
+                        let token = jwt.sign({data: result}, 'secret')
+                        res.send({status: 1, data: result, token: token});
                     }
                 })
+                } else{
+                    //codeEmail incorrecto
+                    res.send({status: 1, data: 'error'});
+                }
+                
             } else{
-                //error porque existe usuario
-                res.send({status: 1, data: 'existente'});
+                //error porque no existe usuario
+                res.send({status: 1, data: 'error'});
             }
         });
     } catch(error){
