0e1d226102f2a0b45cf2efc605b857ab9f63453c
Rodolfo <rodolfo.singular@gmail.com>
2022-12-02 18:36:14 -0300

    Actualización con cloud

--- app.js
+++ app.js
@@ -1,59 +1,3 @@
-/*const express = require('express');
-const indexRouter = require('./routes/index');
-const port = process.env.PORT || 4000;
-const path = require('path');
-const cors = require('cors');
-const fs = require("fs");
-
-const http = require('http');
-const hostname = '127.0.0.1';
-
-//inicialización
-const app = express();
-app.use(express.json({limit: '10mb'}));
-app.use(express.static('public'));
-
-//setting
-app.set('port', port);
-
-//middlewares
-app.use(cors({origin: "*"}));
-app.use(express.json());
-
-app.use('/', indexRouter);
-
-app.get('/prueba', (req, res, next) => {
-    res.send('Exito');
-});
-<<<<<<< HEAD
-=======
-
-//Esto es un cambio para que se guarde en la rama "secondary"
->>>>>>> 1ebc2edb41f39b59ca7a034cfbe559e7f79db043
-
-//Crea la conexión con express directo
-/*
-app.listen(app.get('port'), (err) => {
-    if(err){
-        console.log(`Error del servidor: ${err}`);
-    } else{
-        console.log('Escuchando en el puerto 4000');
-    }
-<<<<<<< HEAD
-
-}) */
-
-//Crea el servidor http que llama a app de express
-/*
-const server = http.createServer(app);
-server.listen(app.get('port'), hostname, (err) => {
-        if(err){
-                console.log(`Error del servidor: ${err}`);
-        } else{
-                console.log(`Servidor corriendo en http://${hostname}`);
-        }
-});
-*/
 
 /* ----------------------------------------------- */
 const express = require('express');
@@ -90,7 +34,3 @@ app.use('/', indexRouter);
 app.get('/prueba', (req, res) => {
     res.send('Exito');
 });
-=======
-    
-})
->>>>>>> 1ebc2edb41f39b59ca7a034cfbe559e7f79db043
